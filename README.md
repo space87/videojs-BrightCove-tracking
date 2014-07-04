04266412aa064ce227b385f913fcb22429798a05
Jason Staerck <jason.staerck@drllimited.co.uk>
2014-07-04 15:58:47 +0100

    Adding the videoTracking.js file and updating the readme file

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-videojs-BrightCove-tracking
-===========================
+# videojs BrightCove tracking
+
+BrightCove plugin for video.js
+
+## Getting Started
+Download [videojs](http://www.videojs.com/) and [videojs.brightcove tracking](https://github.com/space87/videojs-BrightCove-tracking)
+
+On your page include the plugin file.
+```html
+<script src="dist/videojs.ga.min.js"></script>
+```
+
+_Please note that the videojs.js file needs to be loaded prior to this plugin
+
+_Also this can only be used if you call videojs dynamically
+
+you Call the plugin like this
+```javascript
+
+videojs('video', {plugins:{aoBcTracking:{accountID:'Your brightcove accound id',video:{name:'name of your video',id:'id of your brightcove video'}}}}, function() {
+ 
+    //declare your source
+    this.src('example.mp4');
+    
+});
+
+```
+
+_there is no need to call .play() as the plugin will call that to allow tracking of events.
+
+
+## Options
+
+You need provide to provide it with you BrightCove Id in the accountID value.
+
+Also to allow BrightCove to see what video it is tracking you need to put in the video name and id too.
+
+
+
+####events that are tracked
+currently this plugin will track:
+
+"player_load"
+"video_impression"
+"video_view"
+"view_engagement" which reports every 10 seconds allowing the collection of engagement data
+
+
+## TODO
+
+- [ ] Allow custom user tracking to the videos
+- [ ] Track the geoloaction data of the viewer and report it
---
+++ videoTracking.js
@@ -0,0 +1,101 @@
+videojs.plugin("aoBcTracking", function (options) {
+        var player,
+            firstUpdate = true,
+            initialPosition = 0,
+            lastPosition = 0,
+            baseURL = "http://metrics.brightcove.com/tracker?",
+            destination = encodeURI(window.location.href),
+            source = encodeURI(document.referrer),
+            timeUpdate,
+            postData,
+            sendDataToBC,
+            init;
+
+        var settings = {
+            accountID: null,
+            video: {}
+        };
+
+
+        postData = function (requestURL) {
+
+            $.get(requestURL);
+
+            return true;
+        };
+
+        sendDataToBC = function (eventType, evt) {
+            var urlStr = "",
+            time = evt.timeStamp,
+            dateTime = new Date(parseInt(evt.timeStamp)),
+
+            currentVideo = settings.video;
+
+            urlStr = "event=" + eventType + "&domain=videocloud&account=" +  encodeURI(settings.accountID) + "&time=" + time + "&destination=" + encodeURI(destination);
+
+            if (source !== "") {
+                urlStr += "&source=" + encodeURI(source);
+            }
+
+
+
+            if (eventType === "video_impression" || eventType === "video_view" || eventType === "video_engagement") {
+                urlStr += "&video=" + encodeURI(currentVideo.id) + "&video_name=" + encodeURI(currentVideo.name);
+            }
+
+            if (eventType === "video_engagement") {
+                urlStr += "&video_duration=" + player.duration() + "&range=" + evt.range;
+            }
+
+            urlStr = baseURL + urlStr;
+
+            postData(urlStr);
+
+
+            return;
+        };
+        timeUpdate = function (evt) {
+            var thisPosition = evt.timeStamp, range = "", dateTime = new Date(evt.timeStamp);
+            if (firstUpdate) {
+                initialPosition = evt.timeStamp;
+                lastPosition = evt.timeStamp;
+                firstUpdate = false;
+            }
+            if (Math.round(thisPosition / 1000) - Math.round(lastPosition / 1000) === 10) {
+
+                range = ((lastPosition - initialPosition) / 1000).toString() + ".." + ((thisPosition - initialPosition) / 1000).toString();
+
+                lastPosition = thisPosition;
+                evt.range = range;
+
+
+                sendDataToBC("video_engagement", evt);
+            }
+        };
+
+
+        init = function () {
+            player.on("loadstart", function (evt) {
+                sendDataToBC("player_load", evt);
+                firstUpdate = true;
+                sendDataToBC("video_impression", evt);
+            });
+
+            player.on('play',function(evt) {
+                sendDataToBC("video_view", evt);
+            });
+
+            player.on("timeupdate", timeUpdate);
+
+            player.on('loadeddata',function() {
+              player.play();
+            })
+
+        };
+
+        // starting actions
+        $.extend(settings,options);
+        player = this;
+        init();
+        return;
+    });
