d3ad1b14498e3bc2207b46293d26ada7f012c699
bfriederich <friederich.brice@gmail.com>
2015-10-14 11:06:21 +0200

    - refactor the way engagement is sent (buffer) - add secondsViewInterval to control the engagement send interval - don't play automatically the video when videojs is loaded (always use the autoplay value from videojs) - don't send the player_load event since we are not using a player from videocloud

--- README.md
+++ README.md
@@ -9,48 +9,54 @@ On your page include the plugin file.
 ```html
 <script src="dist/videoTracking.min.js"></script>
 ```
-
--Please note that the videojs.js file needs to be loaded prior to this plugin
+-Please note that the **videojs.js** file and **jquery** needs to be loaded prior to this plugin
 
 -Also this can only be used if you call videojs dynamically
 
-you Call the plugin like this
+You Call the plugin like this :
+
 ```javascript
 
-videojs('video', {plugins:{aoBcTracking:{accountID:'Your brightcove accound id',video:{name:'name of your video',id:'id of your brightcove video'}}}}, function() {
- 
-    //declare your source
+videojs('video-offre', {
+    'controls': true,
+    'preload': 'auto',
+    'autoplay': false,
+    'plugins': {
+        brightCoveAnalytics: {
+            accountID: 'Your brightcove accound id',
+            video: {
+                id: 'id of your brightcove video',
+                name: 'name of your video'
+            }
+        }
+    }
+}, function () {
+    // You must declare your source
     this.src('example.mp4');
-    
 });
 
 ```
 
--There is no need to call .play() as the plugin will call that to allow tracking of events.
-
-
 ## Options
 
-You need provide to provide it with you BrightCove Id in the accountID value.
+You need provide to provide it with your BrightCove Id in the accountID value.
 
-Also to allow BrightCove to see what video it is tracking you need to put in the video name and id too.
+Also to allow BrightCove to see what video it is tracking you need to put in the video name and id.
 
+## Tracked events
+Currently this plugin will track:
 
+* video_impression
+* video_view
+* video_engagement : reports every 5 seconds by default, allowing the collection of engagement data. 
+You can change this behavior with the setting secondsViewInterval. The minimum is 5, and the maximum is 20 (otherwise it will be discarded by the brightcove analytics system)
 
-####events that are tracked
-currently this plugin will track:
+You can then see your stats on the videocloud interface, in the analytics section (it takes about 15 minutes for the hits sent to be processed by the brightcove analytics system)
 
-"player_load",
-"video_impression",
-"video_view",
-"view_engagement" which reports every 10 seconds allowing the collection of engagement data
+<img src="doc/engagement.jpg"/>
 
+**The event player_load is not tracked, since you are not using a player from the BrightCove videocloud (which have a playerID), but videojs instead**
 
-## TODO
+## Events api overview (from Brightcove) : 
 
-- [ ] Allow custom user tracking to the videos
-- [ ] Track the geoloaction data of the viewer and report it
-
-
-#####
-Implemneted at ao.com
+http://docs.brightcove.com/en/video-cloud/analytics-api/getting-started/data-collection-api-overview.html
--- videoTracking.js
+++ videoTracking.js
@@ -1,100 +1,141 @@
-videojs.plugin("aoBcTracking", function (options) {
-        var player,
-            firstUpdate = true,
-            initialPosition = 0,
-            lastPosition = 0,
-            baseURL = "http://metrics.brightcove.com/tracker?",
-            destination = encodeURI(window.location.href),
-            source = encodeURI(document.referrer),
-            timeUpdate,
-            postData,
-            sendDataToBC,
-            init;
+videojs.plugin("brightCoveAnalytics", function (options) {
+    var lastTime = -1,
+        duration = -1,
+        baseURL = "http://metrics.brightcove.com/tracker",
+        destination = encodeURI(window.location.href),
+        source = encodeURI(document.referrer),
+        timeBuffer = [],
+        self = this;
 
-        var settings = {
-            accountID: null,
-            video: {}
-        };
+    var settings = {
+        accountID: null,
+        video: {
+            id: null,
+            name: null
+        },
+        secondsViewInterval: 5
+    };
 
+    var timeUpdate = function () {
+        var currentTime = Math.floor(this.currentTime()); // round down to keep only viewed seconds
 
-        postData = function (requestURL) {
+        // Buffering time viewed : buffer starts at 0 and ends at duration minus one second
+        if (lastTime != currentTime && currentTime != duration) {
+            timeBuffer.push(currentTime);
 
-            $.get(requestURL);
-
-            return true;
-        };
-
-        sendDataToBC = function (eventType, evt) {
-            var urlStr = "",
-            time = evt.timeStamp,
-            dateTime = new Date(parseInt(evt.timeStamp)),
-
-            currentVideo = settings.video;
-
-            urlStr = "event=" + eventType + "&domain=videocloud&account=" +  encodeURI(settings.accountID) + "&time=" + time + "&destination=" + encodeURI(destination);
-
-            if (source !== "") {
-                urlStr += "&source=" + encodeURI(source);
+            // If currentTime - lastTime != 1 : an event that jumps time occurs (ex : seek), so let that event handle the buffer
+            if ((currentTime - lastTime) == 1 && timeBuffer.length == settings.secondsViewInterval) {
+                var range = timeBuffer[0] + '..' + timeBuffer[timeBuffer.length - 1];
+                sendVideoEngagement(range);
+                timeBuffer = [];
             }
+        }
 
+        lastTime = currentTime;
+    };
 
-
-            if (eventType === "video_impression" || eventType === "video_view" || eventType === "video_engagement") {
-                urlStr += "&video=" + encodeURI(currentVideo.id) + "&video_name=" + encodeURI(currentVideo.name);
-            }
-
-            if (eventType === "video_engagement") {
-                urlStr += "&video_duration=" + player.duration() + "&range=" + evt.range;
-            }
-
-            urlStr = baseURL + urlStr;
-
-            postData(urlStr);
-
-            return;
+    var sendVideoEngagement = function (range) {
+        var params = {
+            video: settings.video.id,
+            video_name: encodeURI(settings.video.name),
+            video_duration: duration,
+            range: range
         };
-        timeUpdate = function (evt) {
-            var thisPosition = evt.timeStamp, range = "", dateTime = new Date(evt.timeStamp);
-            if (firstUpdate) {
-                initialPosition = evt.timeStamp;
-                lastPosition = evt.timeStamp;
-                firstUpdate = false;
-            }
-            if (Math.round(thisPosition / 1000) - Math.round(lastPosition / 1000) === 10) {
+        sendEvent('video_engagement', params);
+    };
 
-                range = ((lastPosition - initialPosition) / 1000).toString() + ".." + ((thisPosition - initialPosition) / 1000).toString();
+    var sendEvent = function (eventName, params) {
+        params.domain = 'videocloud';
+        params.account = settings.accountID;
+        params.time = Date.now();
+        params.event = eventName;
+        params.destination = destination;
+        if (source !== "") {
+            params.source = source;
+        }
 
-                lastPosition = thisPosition;
-                evt.range = range;
+        $.get(baseURL + '?' + $.param(params));
+    };
 
+    var init = function () {
+        if (settings.accountID == null) {
+            console.log("accountID cannot be null");
+            return false;
+        }
+        if (settings.video.id == null) {
+            console.log("video.id cannot be null");
+            return false;
+        }
+        else if (settings.video.name == null) {
+            console.log("video.name cannot be null");
+            return false;
+        }
+        if (settings.secondsViewInterval < 5) {
+            console.log("secondsViewInterval cannot be inferior to 5 (avoid too much events)");
+            return false;
+        }
+        if (settings.secondsViewInterval > 20) {
+            console.log("secondsViewInterval cannot be superior to 20 (will be discarded by analytics system)");
+            return false;
+        }
+        return true;
+    };
 
-                sendDataToBC("video_engagement", evt);
-            }
-        };
+    $.extend(settings, options);
+    if (!init()) return;
 
-
-        init = function () {
-            player.on("loadstart", function (evt) {
-                sendDataToBC("player_load", evt);
-                firstUpdate = true;
-                sendDataToBC("video_impression", evt);
-            });
-
-            player.on('play',function(evt) {
-                sendDataToBC("video_view", evt);
-            });
-
-            player.on("timeupdate", timeUpdate);
-
-            player.on('loadeddata',function() {
-              player.play();
-            })
-
-        };
-
-        // starting actions
-        $.extend(settings,options);
-        player = this;
-        init();
-        return;
+    self.on('loadeddata', function () {
+        sendEvent('video_impression', {
+            video: settings.video.id,
+            video_name: encodeURI(settings.video.name)
+        });
+        duration = Math.floor(this.duration());
     });
+
+    self.on('firstplay', function () {
+        sendEvent('video_view', {
+            video: settings.video.id,
+            video_name: encodeURI(settings.video.name),
+            video_duration: duration
+        });
+    });
+
+    /** Called by videoJS every 15-250 milliseconds */
+    self.on("timeupdate", timeUpdate);
+
+    self.on('pause', function () {
+        /**
+         * Pause can be triggered if video is already paused and a seek event occurs.
+         * Send buffer if at least one second is viewed and if time is continuous between the last seconds viewed (i.e no seek happened)
+         */
+        if (timeBuffer.length > 1 && (timeBuffer[timeBuffer.length - 1] - timeBuffer[timeBuffer.length - 2]) == 1) {
+            var range = timeBuffer[0] + '..' + timeBuffer[timeBuffer.length - 1];
+            sendVideoEngagement(range);
+            timeBuffer = [];
+        }
+    });
+
+    self.on('seeked', function () {
+        var seekedTime = Math.floor(this.currentTime()); // here currentTime equals the seeked time
+
+        // Send buffer if at least one second has been viewed
+        if (timeBuffer.length > 1) {
+            var lastSecondViewed; // last second viewed before the seekedTime
+            for (var i = 0; i < timeBuffer.length; i++) {
+                if (i == 0) {
+                    if (timeBuffer[i] != seekedTime) {
+                        lastSecondViewed = timeBuffer[i];
+                    }
+                }
+                else {
+                    if (timeBuffer[i] - timeBuffer[i - 1] == 1) { // time is continuous between the last seconds viewed
+                        lastSecondViewed = timeBuffer[i];
+                    }
+                }
+            }
+            var range = timeBuffer[0] + '..' + lastSecondViewed;
+            sendVideoEngagement(range);
+            timeBuffer = [seekedTime];
+        }
+    });
+});
